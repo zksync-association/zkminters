1f04338b692cc58bca8238196b424c25ccc9b0b3
Aditya Anand M C <aditya.anandmc@gmail.com>
2025-07-25 16:54:30 +0530

    feat: implement ZkMinterERC1155V1Factory

---
+++ src/ZkMinterERC1155EligibilityV1Factory.sol
@@ -0,0 +1,158 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {L2ContractHelper} from "src/lib/L2ContractHelper.sol";
+import {ZkMinterERC1155EligibilityV1} from "src/ZkMinterERC1155EligibilityV1.sol";
+import {IZkMinterV1Factory} from "src/interfaces/IZkMinterV1Factory.sol";
+import {IMintable} from "src/interfaces/IMintable.sol";
+
+/// @title ZkMinterERC1155EligibilityV1Factory
+/// @author [ScopeLift](https://scopelift.co)
+/// @notice Factory contract to deploy `ZkMinterERC1155EligibilityV1` contracts using CREATE2. This factory enables
+/// deterministic deployment of ERC1155-based minting contracts with predictable addresses. The factory
+/// supports two deployment methods:
+/// - Standard deployment with explicit parameters
+/// - Unified deployment with encoded parameters for multi-factory compatibility
+/// @dev ZkMinterERC1155EligibilityV1 deployment flow:
+/// 1. Call createMinter() with mintable, admin, erc1155, tokenId, balanceThreshold, and salt nonce
+/// 2. Factory calculates salt using chain ID + nonce and creates a new ZkMinterERC1155EligibilityV1 contract using
+/// CREATE2
+/// 3. Event emitted with deployment details
+/// 4. Address can be predicted using getMinter() before deployment
+/// @custom:security-contact security@matterlabs.dev
+contract ZkMinterERC1155EligibilityV1Factory is IZkMinterV1Factory {
+  /*///////////////////////////////////////////////////////////////
+                  Immutable Variables
+  //////////////////////////////////////////////////////////////*/
+
+  /// @dev Bytecode hash is derived at deployment time from the compiled contract bytecode.
+  bytes32 public immutable BYTECODE_HASH;
+
+  /*///////////////////////////////////////////////////////////////
+                          Events
+  //////////////////////////////////////////////////////////////*/
+
+  /// @notice Emitted when a new `ZkMinterERC1155EligibilityV1` is created.
+  /// @param minterERC1155 The address of the newly deployed `ZkMinterERC1155EligibilityV1`.
+  /// @param mintable A contract used as a target when calling mint.
+  /// @param admin The address that will have admin privileges.
+  /// @param erc1155 The ERC1155 contract to use for balance checks.
+  /// @param tokenId The token ID within the ERC1155 contract.
+  /// @param balanceThreshold The minimum balance required to mint.
+  event MinterERC1155EligibilityCreated(
+    address indexed minterERC1155,
+    IMintable mintable,
+    address admin,
+    address erc1155,
+    uint256 tokenId,
+    uint256 balanceThreshold
+  );
+
+  /*///////////////////////////////////////////////////////////////
+                          Constructor
+  //////////////////////////////////////////////////////////////*/
+
+  /// @notice Initializes the factory with the bytecode hash of the `ZkMinterERC1155EligibilityV1` contract.
+  /// @param _bytecodeHash The bytecode hash of the `ZkMinterERC1155EligibilityV1` contract to be used for CREATE2
+  /// deployments.
+  constructor(bytes32 _bytecodeHash) {
+    BYTECODE_HASH = _bytecodeHash;
+  }
+
+  /*///////////////////////////////////////////////////////////////
+                          External Functions
+  //////////////////////////////////////////////////////////////*/
+
+  /// @notice Deploys a new `ZkMinterERC1155EligibilityV1` contract using CREATE2.
+  /// @param _mintable A contract used as a target when calling mint.
+  /// @param _admin The address that will have admin privileges.
+  /// @param _erc1155 The ERC1155 contract to use for balance checks.
+  /// @param _tokenId The token ID within the ERC1155 contract.
+  /// @param _balanceThreshold The minimum balance required to mint.
+  /// @param _saltNonce A user-provided nonce for salt calculation.
+  /// @return _minterERC1155Address The address of the newly deployed `ZkMinterERC1155EligibilityV1`.
+  function createMinter(
+    IMintable _mintable,
+    address _admin,
+    address _erc1155,
+    uint256 _tokenId,
+    uint256 _balanceThreshold,
+    uint256 _saltNonce
+  ) external returns (address _minterERC1155Address) {
+    _minterERC1155Address = _createMinter(_mintable, _admin, _erc1155, _tokenId, _balanceThreshold, _saltNonce);
+  }
+
+  /// @notice Deploys a new `ZkMinterERC1155EligibilityV1` contract using `CREATE2`. This method takes a bytes argument
+  /// and is meant to be used in a unified factory for all capped minter extensions.
+  /// @param _mintable A contract used as a target when calling mint.
+  /// @param _args The encoded args to deploy `ZkMinterERC1155EligibilityV1`.
+  /// @return The address of the newly deployed `ZkMinterERC1155EligibilityV1`.
+  function createMinter(IMintable _mintable, bytes memory _args) external returns (address) {
+    (address _admin, address _erc1155, uint256 _tokenId, uint256 _balanceThreshold, uint256 _saltNonce) =
+      abi.decode(_args, (address, address, uint256, uint256, uint256));
+    return _createMinter(_mintable, _admin, _erc1155, _tokenId, _balanceThreshold, _saltNonce);
+  }
+
+  /// @notice Computes the address of a `ZkMinterERC1155EligibilityV1` deployed via this factory.
+  /// @param _mintable A contract used as a target when calling mint.
+  /// @param _admin The address that will have admin privileges.
+  /// @param _erc1155 The ERC1155 contract to use for balance checks.
+  /// @param _tokenId The token ID within the ERC1155 contract.
+  /// @param _balanceThreshold The minimum balance required to mint.
+  /// @param _saltNonce The nonce used for salt calculation.
+  /// @return _minterERC1155Address The address of the `ZkMinterERC1155EligibilityV1`.
+  function getMinter(
+    IMintable _mintable,
+    address _admin,
+    address _erc1155,
+    uint256 _tokenId,
+    uint256 _balanceThreshold,
+    uint256 _saltNonce
+  ) external view returns (address _minterERC1155Address) {
+    bytes32 _salt = _calculateSalt(_saltNonce);
+    _minterERC1155Address = L2ContractHelper.computeCreate2Address(
+      address(this),
+      _salt,
+      BYTECODE_HASH,
+      keccak256(abi.encode(_mintable, _admin, _erc1155, _tokenId, _balanceThreshold))
+    );
+  }
+
+  /*///////////////////////////////////////////////////////////////
+                          Internal Functions
+  //////////////////////////////////////////////////////////////*/
+
+  /// @notice Calculates the salt for CREATE2 deployment.
+  /// @param _saltNonce A user-provided nonce for additional uniqueness.
+  /// @return The calculated salt as a bytes32 value.
+  function _calculateSalt(uint256 _saltNonce) internal view returns (bytes32) {
+    return keccak256(abi.encode(block.chainid, _saltNonce));
+  }
+
+  /// @notice Creates a new `ZkMinterERC1155EligibilityV1` contract using CREATE2.
+  /// @param _mintable A contract used as a target when calling mint.
+  /// @param _admin The address that will have admin privileges.
+  /// @param _erc1155 The ERC1155 contract to use for balance checks.
+  /// @param _tokenId The token ID within the ERC1155 contract.
+  /// @param _balanceThreshold The minimum balance required to mint.
+  /// @param _saltNonce A user-provided nonce for salt calculation.
+  /// @return _minterERC1155Address The address of the newly deployed `ZkMinterERC1155EligibilityV1`.
+  function _createMinter(
+    IMintable _mintable,
+    address _admin,
+    address _erc1155,
+    uint256 _tokenId,
+    uint256 _balanceThreshold,
+    uint256 _saltNonce
+  ) internal returns (address _minterERC1155Address) {
+    bytes32 _salt = _calculateSalt(_saltNonce);
+
+    ZkMinterERC1155EligibilityV1 instance =
+      new ZkMinterERC1155EligibilityV1{salt: _salt}(_mintable, _admin, _erc1155, _tokenId, _balanceThreshold);
+    _minterERC1155Address = address(instance);
+
+    emit MinterERC1155EligibilityCreated(
+      _minterERC1155Address, _mintable, _admin, _erc1155, _tokenId, _balanceThreshold
+    );
+  }
+}
---
+++ test/ZkMinterERC1155EligibilityV1Factory.t.sol
@@ -0,0 +1,225 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {Test} from "forge-std/Test.sol";
+import {ZkMinterERC1155EligibilityV1Factory} from "src/ZkMinterERC1155EligibilityV1Factory.sol";
+import {ZkMinterERC1155EligibilityV1} from "src/ZkMinterERC1155EligibilityV1.sol";
+import {IMintable} from "src/interfaces/IMintable.sol";
+import {stdJson} from "forge-std/StdJson.sol";
+import {HashIsNonZero} from "era-contracts/system-contracts/contracts/SystemContractErrors.sol";
+import {FakeERC1155} from "test/fakes/FakeERC1155.sol";
+
+contract ZkMinterERC1155EligibilityV1FactoryTest is Test {
+  bytes32 bytecodeHash;
+  ZkMinterERC1155EligibilityV1Factory factory;
+  FakeERC1155 fakeERC1155;
+
+  function setUp() public virtual {
+    // Read the bytecode hash from the JSON file
+    string memory root = vm.projectRoot();
+    string memory path =
+      string.concat(root, "/zkout/ZkMinterERC1155EligibilityV1.sol/ZkMinterERC1155EligibilityV1.json");
+    string memory json = vm.readFile(path);
+    bytecodeHash = bytes32(stdJson.readBytes(json, ".hash"));
+
+    factory = new ZkMinterERC1155EligibilityV1Factory(bytecodeHash);
+    fakeERC1155 = new FakeERC1155();
+  }
+
+  function _assumeValidAddress(address _addr) internal view {
+    vm.assume(_addr != address(0) && _addr != address(factory));
+  }
+
+  function _assumeValidBalanceThreshold(uint256 _balanceThreshold) internal pure {
+    vm.assume(_balanceThreshold != 0);
+  }
+}
+
+contract CreateMinterERC1155 is ZkMinterERC1155EligibilityV1FactoryTest {
+  function testFuzz_CreatesNewMinterERC1155(
+    IMintable _mintable,
+    address _minterAdmin,
+    address _erc1155,
+    uint256 _tokenId,
+    uint256 _balanceThreshold,
+    uint256 _saltNonce
+  ) public {
+    _assumeValidAddress(_minterAdmin);
+    _assumeValidBalanceThreshold(_balanceThreshold);
+
+    address _minterAddress =
+      factory.createMinter(_mintable, _minterAdmin, _erc1155, _tokenId, _balanceThreshold, _saltNonce);
+
+    ZkMinterERC1155EligibilityV1 _minter = ZkMinterERC1155EligibilityV1(_minterAddress);
+
+    assertEq(address(_minter.mintable()), address(_mintable));
+    assertTrue(_minter.hasRole(_minter.DEFAULT_ADMIN_ROLE(), _minterAdmin));
+    assertEq(address(_minter.ERC1155()), _erc1155);
+    assertEq(_minter.tokenId(), _tokenId);
+    assertEq(_minter.balanceThreshold(), _balanceThreshold);
+  }
+
+  function testFuzz_EmitsMinterERC1155EligibilityCreatedEvent(
+    IMintable _mintable,
+    address _minterAdmin,
+    address _erc1155,
+    uint256 _tokenId,
+    uint256 _balanceThreshold,
+    uint256 _saltNonce
+  ) public {
+    _assumeValidAddress(_minterAdmin);
+    _assumeValidBalanceThreshold(_balanceThreshold);
+
+    address _expectedMinterAddress =
+      factory.getMinter(_mintable, _minterAdmin, _erc1155, _tokenId, _balanceThreshold, _saltNonce);
+
+    vm.expectEmit();
+    emit ZkMinterERC1155EligibilityV1Factory.MinterERC1155EligibilityCreated(
+      _expectedMinterAddress, _mintable, _minterAdmin, _erc1155, _tokenId, _balanceThreshold
+    );
+
+    factory.createMinter(_mintable, _minterAdmin, _erc1155, _tokenId, _balanceThreshold, _saltNonce);
+  }
+
+  function testFuzz_CreatesNewMinterERC1155WithBytesArgs(
+    IMintable _mintable,
+    address _minterAdmin,
+    address _erc1155,
+    uint256 _tokenId,
+    uint256 _balanceThreshold,
+    uint256 _saltNonce
+  ) public {
+    _assumeValidAddress(_minterAdmin);
+    _assumeValidBalanceThreshold(_balanceThreshold);
+
+    address _minterAddress =
+      factory.createMinter(_mintable, abi.encode(_minterAdmin, _erc1155, _tokenId, _balanceThreshold, _saltNonce));
+
+    ZkMinterERC1155EligibilityV1 _minter = ZkMinterERC1155EligibilityV1(_minterAddress);
+
+    assertEq(address(_minter.mintable()), address(_mintable));
+    assertTrue(_minter.hasRole(_minter.DEFAULT_ADMIN_ROLE(), _minterAdmin));
+    assertEq(address(_minter.ERC1155()), _erc1155);
+    assertEq(_minter.tokenId(), _tokenId);
+    assertEq(_minter.balanceThreshold(), _balanceThreshold);
+  }
+
+  function testFuzz_EmitsMinterERC1155EligibilityCreatedEventWithBytesArgs(
+    IMintable _mintable,
+    address _minterAdmin,
+    address _erc1155,
+    uint256 _tokenId,
+    uint256 _balanceThreshold,
+    uint256 _saltNonce
+  ) public {
+    _assumeValidAddress(_minterAdmin);
+    _assumeValidBalanceThreshold(_balanceThreshold);
+
+    address _expectedMinterAddress =
+      factory.getMinter(_mintable, _minterAdmin, _erc1155, _tokenId, _balanceThreshold, _saltNonce);
+
+    vm.expectEmit();
+    emit ZkMinterERC1155EligibilityV1Factory.MinterERC1155EligibilityCreated(
+      _expectedMinterAddress, _mintable, _minterAdmin, _erc1155, _tokenId, _balanceThreshold
+    );
+
+    factory.createMinter(_mintable, abi.encode(_minterAdmin, _erc1155, _tokenId, _balanceThreshold, _saltNonce));
+  }
+
+  function testFuzz_RevertIf_CreatingDuplicateMinter(
+    IMintable _mintable,
+    address _minterAdmin,
+    address _erc1155,
+    uint256 _tokenId,
+    uint256 _balanceThreshold,
+    uint256 _saltNonce
+  ) public {
+    _assumeValidAddress(_minterAdmin);
+    _assumeValidBalanceThreshold(_balanceThreshold);
+
+    factory.createMinter(_mintable, abi.encode(_minterAdmin, _erc1155, _tokenId, _balanceThreshold, _saltNonce));
+
+    vm.expectRevert(abi.encodeWithSelector(HashIsNonZero.selector, bytecodeHash));
+    factory.createMinter(_mintable, _minterAdmin, _erc1155, _tokenId, _balanceThreshold, _saltNonce);
+  }
+
+  function testFuzz_RevertIf_CreatingMinterWithZeroAdmin(
+    IMintable _mintable,
+    address _erc1155,
+    uint256 _tokenId,
+    uint256 _balanceThreshold,
+    uint256 _saltNonce
+  ) public {
+    _assumeValidBalanceThreshold(_balanceThreshold);
+    vm.expectRevert(
+      abi.encodeWithSelector(ZkMinterERC1155EligibilityV1.ZkMinterERC1155EligibilityV1__InvalidZeroAddress.selector)
+    );
+    factory.createMinter(_mintable, address(0), _erc1155, _tokenId, _balanceThreshold, _saltNonce);
+  }
+
+  function testFuzz_RevertIf_CreatingMinterWithZeroBalanceThreshold(
+    IMintable _mintable,
+    address _minterAdmin,
+    address _erc1155,
+    uint256 _tokenId,
+    uint256 _saltNonce
+  ) public {
+    _assumeValidAddress(_minterAdmin);
+    vm.expectRevert(
+      abi.encodeWithSelector(
+        ZkMinterERC1155EligibilityV1.ZkMinterERC1155EligibilityV1__InvalidBalanceThreshold.selector
+      )
+    );
+    factory.createMinter(_mintable, _minterAdmin, _erc1155, _tokenId, 0, _saltNonce);
+  }
+}
+
+contract GetMinter is ZkMinterERC1155EligibilityV1FactoryTest {
+  function testFuzz_ReturnsCorrectMinterAddress(
+    IMintable _mintable,
+    address _minterAdmin,
+    address _erc1155,
+    uint256 _tokenId,
+    uint256 _balanceThreshold,
+    uint256 _saltNonce
+  ) public {
+    _assumeValidAddress(_minterAdmin);
+    _assumeValidBalanceThreshold(_balanceThreshold);
+
+    address _expectedMinterAddress =
+      factory.getMinter(_mintable, _minterAdmin, _erc1155, _tokenId, _balanceThreshold, _saltNonce);
+    address _minterAddress =
+      factory.createMinter(_mintable, abi.encode(_minterAdmin, _erc1155, _tokenId, _balanceThreshold, _saltNonce));
+
+    assertEq(_minterAddress, _expectedMinterAddress);
+  }
+
+  function testFuzz_GetMinterWithoutDeployment(
+    IMintable _mintable,
+    address _minterAdmin,
+    address _erc1155,
+    uint256 _tokenId,
+    uint256 _balanceThreshold,
+    uint256 _saltNonce
+  ) public {
+    _assumeValidAddress(_minterAdmin);
+    _assumeValidBalanceThreshold(_balanceThreshold);
+
+    address _expectedMinterAddress =
+      factory.getMinter(_mintable, _minterAdmin, _erc1155, _tokenId, _balanceThreshold, _saltNonce);
+
+    uint256 _codeSize;
+    assembly {
+      _codeSize := extcodesize(_expectedMinterAddress)
+    }
+    assertEq(_codeSize, 0);
+
+    factory.createMinter(_mintable, abi.encode(_minterAdmin, _erc1155, _tokenId, _balanceThreshold, _saltNonce));
+  }
+}
+
+contract BytecodeHash is ZkMinterERC1155EligibilityV1FactoryTest {
+  function test_BytecodeHashIsSetCorrectly() public view {
+    assertEq(factory.BYTECODE_HASH(), bytecodeHash);
+  }
+}
